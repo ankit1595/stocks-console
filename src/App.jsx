db5c136773945e0121b387cf03aca7bf0e64486b
Ankit <ankit.95sharma@gmail.com>
2022-10-05 13:21:12 +0530

    feat: calculate profit and loss functionality

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [buyingPrice, setBuyingPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [currentPrice, setCurrentPrice] = useState("");
+  const [output, setOutput] = useState("");
+  const [textColor, setTextColor] = useState("black");
+
+  function handleBuyingPriceInput(e) {
+    setBuyingPrice(e.target.value);
+    setOutput("");
+  }
+
+  function handleQuantityInput(e) {
+    setQuantity(e.target.value);
+    setOutput("");
+  }
+
+  function handleCurrentPriceInput(e) {
+    setCurrentPrice(e.target.value);
+    setOutput("");
+  }
+
+  function submitForm() {
+    setOutput("");
+    if (buyingPrice <= 0 || quantity <= 0 || currentPrice <= 0) {
+      setOutput("Invalid Input");
+    } else {
+      //calculate Profit and loss
+      // loss
+      if (buyingPrice > currentPrice) {
+        let loss = ((buyingPrice - currentPrice) * quantity).toFixed(2);
+        let lossPercentage = ((loss / (buyingPrice * quantity)) * 100).toFixed(2);
+        setTextColor("red")
+        setOutput(
+          `You have loss of ${loss} and loss Percentage is ${lossPercentage}% .`
+        );
+      }
+      // profit
+      else if (buyingPrice < currentPrice) {
+        let profit = ((currentPrice - buyingPrice) * quantity).toFixed(2);
+        let profitPercentage = ((profit / (buyingPrice * quantity)) * 100).toFixed(2);
+        setTextColor("green");
+
+        setOutput(
+          `You gain a Profit of ${profit} and profit Percentage is ${profitPercentage}%`
+        );
+      }
+      // No profit, No loss
+      else {
+        setTextColor("black");
+        setOutput(`No profit, No loss`);
+      }
+    }
+  }
+
+  function resetForm() {
+    setBuyingPrice("");
+    setQuantity("");
+    setCurrentPrice("");
+    setOutput("");
+  }
+
+  return (
+    <div className="App">
+      <h1>Stocks Console</h1>
+      <label>
+        Buying Price:
+        <input
+          type="number"
+          placeholder="Enter Cost Price"
+          onChange={handleBuyingPriceInput}
+          value={buyingPrice}
+        />
+      </label>
+      <label>
+        Stocks Quantity
+        <input
+          type="number"
+          placeholder="Enter Number of Stocks"
+          onChange={handleQuantityInput}
+          value={quantity}
+        />
+      </label>
+      <label>
+        Current Price:
+        <input
+          type="number"
+          placeholder="Enter Current Price"
+          onChange={handleCurrentPriceInput}
+          value={currentPrice}
+        />
+      </label>
+      <button onClick={submitForm}>Calculate</button>
+      <button onClick={resetForm}>Reset</button>
+      <div style={{color: textColor}}>{output}</div>
+    </div>
+  );
+}
+
+export default App;
