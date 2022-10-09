9714085ace0f36775dc9effb31e58a6af517052f
Ankit <ankit.95sharma@gmail.com>
2022-10-09 11:48:43 +0530

    style: styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
 import { useState } from "react";
 
+import twitterLogo from "./images/brand-logos/logo-twitter.svg";
+import githubLogo from "./images/brand-logos/logo-github.svg";
+import linkedinLogo from "./images/brand-logos/logo-linkedin.svg";
+import mailLogo from "./images/brand-logos/mail.svg";
+import angelLogo from "./images/brand-logos/angel-icon.svg";
+
 function App() {
   const [buyingPrice, setBuyingPrice] = useState("");
   const [quantity, setQuantity] = useState("");
@@ -30,7 +36,6 @@ function App() {
       setTextColor("red");
       setOutput("Invalid Input");
     } else {
-
       //calculate Profit and loss
       // loss
       if (buyingPrice > currentPrice) {
@@ -106,13 +111,72 @@ function App() {
           />
         </label>
         <button onClick={submitForm}>Calculate</button>
-        <button style={{backgroundColor: "#dc3545", color: "white", borderColor: "#dc3545"}} onClick={resetForm}>
+        <button
+          style={{
+            backgroundColor: "#dc3545",
+            color: "white",
+            borderColor: "#dc3545",
+          }}
+          onClick={resetForm}>
           Reset
         </button>
         <div className="output" style={{ color: textColor }}>
           {output}
         </div>
       </main>
+      <footer className="footer">
+        <div className="footer-header">
+          © | 2022 |{" "}
+          <a
+            className="link"
+            href="http://ankit-sharma15.netlify.app"
+            target={"_blank"}>
+            Ankit Sharma
+          </a>{" "}
+        </div>
+        <ul className="list-non-bullet link-social">
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://twitter.com/ankit1595"
+              target="_blank">
+              <img src={twitterLogo} alt="twitter-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://github.com/ankit1595"
+              target="_blank">
+              <img src={githubLogo} alt="github-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://www.linkedin.com/in/ankit1595"
+              target="_blank">
+              <img src={linkedinLogo} alt="linkedin-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="mailto:ankit.95sharma@gmail.com"
+              target="_blank">
+              <img src={mailLogo} alt="mail-logo" />
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://www.angel.co/u/ankit1595"
+              target="_blank">
+              <img src={angelLogo} alt="angel-logo" />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
